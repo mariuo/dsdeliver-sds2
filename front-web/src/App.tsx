e0e961bb64462e238d90e8aad6a5ad67002009b5
Mario Camelo <mario@mcamelo.org>
2021-01-13 16:22:46 -0300

    Add Navbar

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css';
+import Navbar from './Navbar';
 
 
 
@@ -6,8 +7,7 @@ import './App.css';
 function App() {
   return (
     <div>
-
-      <h1> Hello</h1>
+      <Navbar />
     </div>
   );
 }
